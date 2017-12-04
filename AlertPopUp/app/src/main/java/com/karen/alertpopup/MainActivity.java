e73954261456ecc38e38f6ef73c46fba6aaffffe
Karen Grygoryan <karen@aroundwire.com>
2017-12-04 11:17:55 -0800

    update to alertpopup

--- AlertPopUp/app/src/main/java/com/karen/alertpopup/MainActivity.java
+++ AlertPopUp/app/src/main/java/com/karen/alertpopup/MainActivity.java
@@ -1,29 +1,79 @@
 package com.karen.alertpopup;
 
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.Toast;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private SharedPreferences sharedPreferences;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()) {
+            case R.id.spanish:
+                setLanguage("Spanish");
+                return true;
+            case R.id.english:
+                setLanguage("English");
+                return true;
+            default:
+                return false;
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
 
-        new AlertDialog.Builder(this)
-                .setIcon(android.R.drawable.ic_dialog_alert)
-                .setTitle("Are you sure?")
-                .setMessage("Do you really want to do this?")
-                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Toast.makeText(getApplicationContext(), "It's done!", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .setNegativeButton("No", null)
-                .show();
+        sharedPreferences = this.getSharedPreferences("com.karen.alertpopup", Context.MODE_PRIVATE);
+        String language = sharedPreferences.getString("language", "");
+
+        if (language.isEmpty()) {
+
+            new AlertDialog.Builder(this)
+                    .setIcon(android.R.drawable.ic_btn_speak_now)
+                    .setTitle("Choose as language")
+                    .setMessage("Which language do you like?")
+                    .setPositiveButton("English", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            setLanguage("English");
+                        }
+                    })
+                    .setNegativeButton("Spanish", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            setLanguage("Spanish");
+                        }
+                    })
+                    .show();
+        } else {
+            textView.setText(language);
+        }
+    }
+
+    public void setLanguage(String language) {
+        sharedPreferences.edit().putString("language", language).apply();
+        textView.setText(language);
     }
 }
